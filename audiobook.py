841d155cb65993b2b268804422036105257c8d4c
TANUSHREE1111 <tanushreesarkar18799@gmail.com>
2022-10-14 00:01:08 +0530

    Create audiobook.py

---
+++ audiobook.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import PyPDF2
+
+book= open('Oop.pdf','rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages= pdfReader.numPages
+print(pages)
+speaker = pyttsx3.init()
+page = pdfReader.getPage(1)
+txt = page.extractText()
+speaker.say(txt)
+speaker.runAndWait()
